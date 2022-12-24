d8df94a06ef02cbec03690c21cd0fadb30f624bf
Will Koury <wkoury@hey.com>
2022-12-24 11:10:54 -0500

    Statically generate page for specific types

---
+++ pages/type/[id]/index.tsx
@@ -0,0 +1,74 @@
+import type { GetStaticPaths, GetStaticProps, GetStaticPropsContext, NextPage } from 'next';
+import { Table } from '@mantine/core';
+import db from '../../../db.json';
+import Head from 'next/head';
+
+type Designator = {
+  Designator: string;
+  [x: string]: any; // FIXME
+};
+
+interface TypeProps {
+  types: any[];
+}
+
+const TypePage: NextPage<TypeProps> = ({ types }) => {
+  return (
+    <>
+      <h1>{types[0].Designator}</h1>
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Designator</th>
+            <th>Engine Manufacturer</th>
+          </tr>
+        </thead>
+        <tbody>
+          {types.map((item: any) => (
+            <tr key={types.indexOf(item)}>
+              <td>{item.ModelFullName}</td>
+              <td>{item.Designator}</td>
+              <td>{item.ManufacturerCode}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default TypePage;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  let types = new Set<string>();
+  db.forEach((item: Designator) => {
+    types.add(item.Designator);
+  });
+
+  return {
+    paths: Array.from(types).map((item: string) => ({
+      params: {
+        id: item,
+      },
+    })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+  let id: string | undefined = undefined;
+  if (Array.isArray(context.params?.id)) {
+    id = context.params?.id[0];
+  } else {
+    id = context.params?.id;
+  }
+
+  const types = db.filter((item: any) => item.Designator === id);
+
+  return {
+    props: {
+      types: types,
+    },
+  };
+};
