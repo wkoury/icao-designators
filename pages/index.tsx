e2536aba1b89cda2adeea1464a76e143eee521b7
Will Koury <wkoury@hey.com>
2023-08-17 15:37:58 -0500

    Update types and date

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ const Home: NextPage<HomeProps> = ({ db }) => {
             ))}
           </tbody>
         </Table>
-        <span>Next Database Update: July 14, 2023</span>
+        <span>Next Database Update: September 8, 2023</span>
       </main>
     </>
   );
